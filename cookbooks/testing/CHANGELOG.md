a30314132d410beaba35202807dc7ca456734230
Will Fisher <wfisher@chef.io>
2019-03-21 10:34:28 -0700

    Adding cookbooks
    Signed-off-by: Will Fisher <wfisher@chef.io>

---
+++ cookbooks/testing/CHANGELOG.md
@@ -0,0 +1,11 @@
+# testing CHANGELOG
+
+This file is used to list changes made in each version of the testing cookbook.
+
+# 0.1.0
+
+Initial release.
+
+- change 0
+- change 1
+
---
+++ cookbooks/testing/recipes/default.rb
@@ -0,0 +1,10 @@
+#
+# Cookbook:: testing
+# Recipe:: default
+#
+# Copyright:: 2019, The Authors, All Rights Reserved.
+#
+
+file '/tmp/testing' do
+  content 'this is a test'
+end
